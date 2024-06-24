9f440b39dca9f71c01317a750034df82dca17fba
Nahuel Anselmo <anselmonahuel63t@gmail.com>
2024-06-24 17:54:19 -0300

    Ejercicio 3 terminado nashe

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css';
 // import Ejercicio1 from './Components/Ejercicio1/Ejercicio1';
-import Ejercicio2 from './Components/Ejercicio2/Ejercicio2.jsx';
+// import Ejercicio2 from './Components/Ejercicio2/Ejercicio2.jsx';
+import Ejercicio3 from './Components/Ejercicio3/Ejercicio3';
+
+
 const App = () => {
   return (
     <>
     {/* <Ejercicio1/> */}
-    <Ejercicio2
-    mensaje = 'my friend!'/>
+    {/* <Ejercicio2
+    mensaje = 'my friend!'/> */}
+    <Ejercicio3
+    mensaje = 'my friend!'
+    />
     </>
+
   )
 }
 export default App
